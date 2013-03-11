8a2369d7947bc85114bffdc08b93e9e383cbd69b
luke-dr <ldressel@gmail.com>
2013-03-11 16:40:37 -0700

    Creation of airlines feature complete

--- app/controllers/airlines_controller.rb
+++ app/controllers/airlines_controller.rb
@@ -7,4 +7,18 @@ class AirlinesController < ApplicationController
     @airline = Airline.new
   end
 
+  def create
+    @airline = Airline.new(params[:airline])
+    if @airline.save
+      flash[:notice] = "Airline has been added."
+      redirect_to @airline
+    else
+      # nothing, yet
+    end
+  end
+
+  def show
+    @airline = Airline.find(params[:id])
+  end
+
 end
