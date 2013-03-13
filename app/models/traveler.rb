0dc532ad77481c486fbeaea775a92f6c22610d67
Jayml <jayml.mistry@gmail.com>
2013-03-12 17:55:52 -0700

    Working on editing users. Blank field confirmation failing.

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -4,5 +4,5 @@ class Traveler < ActiveRecord::Base
   has_many :Flights
 
   validates :first_name, :last_name, :presence => true
-  validates :email, :presence => true
+  validates :email, :presence => true, :uniqueness => true
 end
